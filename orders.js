fa83833225f9c1df6cb0d8503442fa896b426903
iamjoelosas <joeldavidenabulele@gmail.com>
2023-09-11 09:52:51 -0400

    first commit

---
+++ orders.js
@@ -0,0 +1,27 @@
+const Orders = [
+    {
+        productName:'Airtel airtime',
+        productNumber:'49347',
+        paymentStatus:'Due',
+        shipping:"Pending"
+     },
+     {
+         productName:'MTN airtime',
+         productNumber:'29785',
+         paymentStatus:'Refunded',
+         shipping:'Declined'
+     },
+     {
+        productName:'GLO airtime',
+        productNumber:'49347',
+        paymentStatus:'Paid',
+        shipping:"Pending"
+     },
+     {
+         productName:'ETISALAT data',
+         productNumber:'96996',
+         paymentStatus:'Due',
+         shipping:'Delivered'
+     },
+     
+]
